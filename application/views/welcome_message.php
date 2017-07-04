05a76d38ae6a17ec2ebdd075d9fdf5ccad85e20a
hmmis <mislam6789@gmail.com>
2017-07-03 19:32:32 -0700

    FTPloy Connected

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -7,5 +7,8 @@
 	<h1>Welcome to CodeIgniter!</h1>
 	<a href="<?php echo base_url()?>my_autocomplete/show" target="_blank">Auto Complete</a><br>
 	<a href="<?php echo base_url()?>my_auto_scroll_load/scroll_pagination" target="_blank">Auto Load On Scroll</a><br>
+
+	<hr>
+	<h4>This Project is created by H M Mohidul Islam Shovon</h4>
 </body>
 </html>
